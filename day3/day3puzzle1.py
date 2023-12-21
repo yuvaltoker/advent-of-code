81b85879556d8e26e6b154aa2dd84bc2f2d63d72
yuval toker <yuvalt2710@gmail.com>
2023-12-21 18:26:33 +0200

    started day3

---
+++ day3/day3puzzle1.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    with open('/home/toker/advent-of-code/day2/day3puzzle1input.txt', 'r') as input_file:
+        file = input_file.read()
+        lines = file.split('\n')
+        
